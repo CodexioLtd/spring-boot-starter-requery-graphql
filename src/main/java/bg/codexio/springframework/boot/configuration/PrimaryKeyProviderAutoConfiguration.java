407ccd0f0f6fc8eebf6c9af1df73e45ad41700c6
Vassil Kamburov <vassil.kamburov@codexio.bg>
2024-09-20 15:54:34 +0300

    - initial set-up for GraphQL Starter configuration

---
+++ src/main/java/bg/codexio/springframework/boot/configuration/PrimaryKeyProviderAutoConfiguration.java
@@ -0,0 +1,14 @@
+package bg.codexio.springframework.boot.configuration;
+
+import bg.codexio.springframework.data.jpa.requery.specification.joinColumn.types.enumType.LongTypePrimaryKeyProvider;
+import bg.codexio.springframework.data.jpa.requery.specification.joinColumn.types.enumType.PrimaryKeyProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PrimaryKeyProviderAutoConfiguration {
+    @Bean
+    public PrimaryKeyProvider primaryKeyProvider() {
+        return new LongTypePrimaryKeyProvider();
+    }
+}
---
+++ src/main/java/bg/codexio/springframework/boot/configuration/SwaggerAutoConfiguration.java
@@ -0,0 +1,28 @@
+package bg.codexio.springframework.boot.configuration;
+
+import bg.codexio.springframework.data.jpa.requery.config.SwaggerConfig;
+import org.springdoc.core.customizers.OperationCustomizer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnWebApplication
+@ConditionalOnClass(name = "org.springdoc.core.customizers.OperationCustomizer")
+public class SwaggerAutoConfiguration {
+    @Bean
+    public SwaggerConfig swaggerConfig() {
+        return new SwaggerConfig();
+    }
+
+    @Bean
+    public OperationCustomizer customizeFiltering(SwaggerConfig swaggerConfig) {
+        return swaggerConfig.customizeFiltering();
+    }
+
+    @Bean
+    public OperationCustomizer customizePageable(SwaggerConfig swaggerConfig) {
+        return swaggerConfig.customizePageable();
+    }
+}
